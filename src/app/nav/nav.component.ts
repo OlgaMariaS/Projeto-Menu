92a304f0e52436fb48f0955e9e57e017fec33178
Olga Maria dos Santos Gonçalves <olgamariasssantos@gmail.com>
2022-03-28 12:58:18 -0300

    ajuste de menu maximizar/minimizar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  qtdNotificacao: number = 0;
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.qtdNotificacao=11;
   }
-
 }
